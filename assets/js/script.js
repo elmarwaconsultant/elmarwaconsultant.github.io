15c28e179f57ac28b1a601f1ec3ec55702c6aa14
elmarwaconsultant <elmarwaconsultant@gmail.com>
2024-10-13 11:03:29 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const partners = document.querySelector('.partners');
-    const prevBtn = document.querySelector('.prev');
-    const nextBtn = document.querySelector('.next');
-  
-    // Slide to the left
-    prevBtn.addEventListener('click', function() {
-      partners.scrollLeft -= 200;
-    });
-  
-    // Slide to the right
-    nextBtn.addEventListener('click', function() {
-      partners.scrollLeft += 200;
-    });
+  const partners = document.querySelector('.partners');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+
+  // Slide to the left
+  prevBtn.addEventListener('click', function() {
+    partners.scrollLeft -= 200;
   });
+
+  // Slide to the right
+  nextBtn.addEventListener('click', function() {
+    partners.scrollLeft += 200;
+  });
+});
+
+// Open the image in full size
+function openFullSize(imgElement) {
+const overlay = document.getElementById('fullscreen-overlay');
+const fullImg = document.getElementById('full-img');
+fullImg.src = imgElement.src;
+overlay.style.display = 'flex';
+document.body.classList.add('blur');
+}
+
+// Close the full-size image and hide the overlay
+function closeFullSize() {
+const overlay = document.getElementById('fullscreen-overlay');
+overlay.style.display = 'none';
+document.body.classList.remove('blur');
+}
